3993354f78bcc8256cd4ff999621df50ee7b92c1
Tiago195 <tiagocf475@gmail.com>
2022-12-06 13:38:43 -0300

    feat: :sparkles: user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permited_parameters 
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :phone)
+    end
+  end
+
 end
