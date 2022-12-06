64618fbfa7b38e0ec8e0116fcc8ca830ee4969d5
Tiago195 <tiagocf475@gmail.com>
2022-12-06 13:05:02 -0300

    test: :white_check_mark:

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    password = Faker::Number.number
+    email { Faker::Internet.email }
+    password { password }
+    password_confirmation { password }
+    name { Faker::Name.name }
+    phone { Faker::PhoneNumber.phone_number }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validating fields" do
+    it "name is not null" do
+      user = build(:user, name: nil)
+
+      expect(user.valid?).to eq(false)
+    end
+    it "phone is not null" do
+      user = build(:user, phone: nil)
+
+      expect(user.valid?).to eq(false)
+    end
+    it "creating user with all valid" do
+      user = build(:user)
+
+      expect(user.valid?).to eq(true)
+    end
+  end
 end
